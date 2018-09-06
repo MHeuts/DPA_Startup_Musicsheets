02e967124a7f2f473469c5e4c424d9d57b8f5869
Dajo Hofman <d.hofman@dj33.nl>
2018-09-06 14:03:28 +0200

    Note -> MusicNote

--- DPA_Musicsheets/Models/Bar.cs
+++ DPA_Musicsheets/Models/Bar.cs
@@ -8,7 +8,7 @@ namespace DPA_Musicsheets.Models
 {
     class Bar
     {
-        public List<Note> MusicNotes { get; set; }
+        public List<MusicNote> MusicNotes { get; set; }
         public Tuple<int, int> Rhythm { get; set; }
         public int Bpm { get; set; }
     }
