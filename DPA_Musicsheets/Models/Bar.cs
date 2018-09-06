0515ccc407b7947baf4431325a0881b749e36597
Dajo Hofman <d.hofman@dj33.nl>
2018-09-06 13:59:04 +0200

    Added basic domain classes

---
+++ DPA_Musicsheets/Models/Bar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Models
+{
+    class Bar
+    {
+        public List<Note> MusicNotes { get; set; }
+        public Tuple<int, int> Rhythm { get; set; }
+        public int Bpm { get; set; }
+    }
+}
