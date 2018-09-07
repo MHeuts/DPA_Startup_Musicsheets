d3a047b65f6f1cd71253023cf64b857d5b360dfd
Dajo Hofman <d.hofman@dj33.nl>
2018-09-07 10:25:12 +0200

    Basic builders

---
+++ DPA_Musicsheets/Models/BarBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Models
+{
+    class BarBuilder
+    {
+        private Bar _bar;
+
+        public BarBuilder()
+        {
+            _bar = new Bar();
+        }
+
+        public AddNote(MusicNote note)
+        {
+            _bar.MusicNotes.Add(note);
+        }
+    }
+}
