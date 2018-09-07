3cd0a07cc6ff731384bf2720e302ab0264b55a71
Dajo Hofman <d.hofman@dj33.nl>
2018-09-07 10:40:50 +0200

    AddSong for builder

--- DPA_Musicsheets/Models/BarBuilder.cs
+++ DPA_Musicsheets/Models/BarBuilder.cs
@@ -15,9 +15,20 @@ namespace DPA_Musicsheets.Models
             _bar = new Bar();
         }
 
-        public AddNote(MusicNote note)
+        public void AddNote(MusicNote note)
         {
             _bar.MusicNotes.Add(note);
+        }
+
+        public void AddNote(char tone, int duration, bool dot, int octave, char modifier)
+        {
+            _bar.MusicNotes.Add(new MusicNote {
+                Tone = tone,
+                Dot = dot,
+                Duration = duration,
+                Modifier = modifier,
+                Octave = octave
+            });
         }
     }
 }
