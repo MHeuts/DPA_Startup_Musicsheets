1a746a580342fe8d65e1bc70df42589b82729e20
Marijn Heuts <m.heuts@live.nl>
2018-10-28 11:06:31 +0100

    changes

---
+++ DPA_Musicsheets/IO/FileHandlers/LilypondFileHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DPA_Musicsheets.Converters.LilyPond;
+using DPA_Musicsheets.IO.FileHandlers;
+using DPA_Musicsheets.Models;
+
+namespace DPA_Musicsheets.IO
+{
+    class LilypondFileHandler : MusicFileHandler
+    {
+        public List<string> Extensions { get; private set; }
+
+        public LilypondFileHandler()
+        {
+            Extensions = new List<string> { "ly" };
+        }
+
+        protected override Staff Load(string filename)
+        {
+            var converter = new LilyPondConverter();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var line in File.ReadAllLines(filename))
+            {
+                sb.AppendLine(line);
+            }
+
+            return converter.ConvertBack(sb.ToString());
+        }
+
+        protected override bool Save(string filename, Staff staff)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
